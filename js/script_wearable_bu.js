3ad74872d5ec4d9f7e1db6155a618d750476bdfc
Christina <cchen17@gmail.com>
2020-01-03 16:32:36 -0500

    Update script_wearable_bu.js

--- js/script_wearable_bu.js
+++ js/script_wearable_bu.js
@@ -1054,20 +1054,3 @@ function getAudio(blob, encoding, filename) {
 function play(url) {
   new Audio(url).play();
 }
-
-function openForm() {
-  document.getElementById("userform").style.width = "100%";
-  setTimeout(showCloser, 500);
-}
-
-function showCloser(){
-  document.getElementById("closer").style.display = "flex";
-  document.getElementById("closer").style.position = "fixed";
-  document.getElementById("closer").style.property = "justify-content: center";
-}
-
-function closeForm() {
-  document.getElementById("userform").style.width = "0%";
-  document.getElementById("closer").style.display = "none";
-
-}
